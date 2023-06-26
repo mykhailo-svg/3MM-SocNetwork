2cfa31a607743e3175e1f610630e494e4c278d70
mykhailo-svg <74141693+mykhailo-svg@users.noreply.github.com>
2023-06-26 11:28:38 +0300

    before_renderer-delete

--- src/state.js
+++ src/state.js
@@ -27,7 +27,7 @@ let state = {
             {id: "2", post_text: "Spent the afternoon painting a beautiful landscape.", likes: 5, comments: 2, time: "15:45"},
             {id: "3", post_text: "I scored a goal in today's soccer match!", likes: 15, comments: 6, time: "18:15"},
             {id: "4", post_text: "Experimented with a new recipe and baked a delicious cake.", likes: 8, comments: 1, time: "12:00"},
-            {id: "5", post_text: "Took my dog for a long walk in the park. Such a fun day!", likes: 20, comments: 4, time: "09:00"}
+            {id: "5", post_text: "Took my dog for a long walk in the park. Such a fun day!", likes: 20, comments: 4, time: "09:00"}, 
         ],
         
         User:{
