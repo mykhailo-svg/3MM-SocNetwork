47294bbd2cc11d572d0e25e5849f268c77ef6426
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-08-19 19:56:25 +1200

    Theme toggler UI

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,11 +1,12 @@
-
+import "./Settings.scss";
 import React from "react";
+import Theme from "./Theme/Theme";
 
 
 const Settings = () => {
     return (
-        <div>
-
+        <div className="settings _container">
+            <Theme />
         </div>
 
 
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,22 @@
+import { fetchUsers } from '../redux/slices/People/people_slice.slice';
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actions } from "../redux/slices/Messages/chat_slice.slice";
+
+
+import { actions as people_actions } from "../redux/slices/People/people_slice.slice";
+import { fetchUserById } from '../redux/slices/Profile/user_slice.slice';
+
+
+
+const rootActions = {
+    ...actions,fetchUsers,fetchUserById
+}
+
+export const useActions = () => {
+    const dispatch = useDispatch()
+
+    return useMemo(() => bindActionCreators(rootActions, dispatch)
+    , [dispatch])
+}
